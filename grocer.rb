809502be51d2f811773c8da4f5d164723d80c167
avid-modal-4107 <norbertl@buffalo.edu>
2018-12-18 22:49:59 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -3,9 +3,15 @@ def consolidate_cart(cart)
   output = {}
   
   cart.each do |item, info|
-    
-    
+    if output.key?(item)
+      output[item][:count] += 1
+    else
+      output[item] = info
+      output[item][:count] = 1
+    end
+  end
   
+  return output
 end
 
 def apply_coupons(cart, coupons)
