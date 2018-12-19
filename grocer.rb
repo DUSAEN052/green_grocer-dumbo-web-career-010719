a965a85d3452a940c5d6797293932c75a8c43ab8
avid-modal-4107 <norbertl@buffalo.edu>
2018-12-19 00:42:31 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -19,7 +19,7 @@ end
 def apply_coupons(cart, coupons)
   # code here
   output = {}
-  
+  puts coupons
   cart.each do |item, info|
     if coupons[:item] == item and coupons[:num] <= info[:count]
       output[item + " W/COUPON"] = {
