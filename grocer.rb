de8f978389d7ee9e24b973fa77f89c879d5c763a
avid-modal-4107 <norbertl@buffalo.edu>
2018-12-19 04:28:58 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -26,7 +26,7 @@ def apply_coupons(cart, coupons)
   cart.each do |item, info|
     
     coupons.each do |coupon|
-      if coupon[:item] == item and coupon[:num] <= info[:count] and output.key?(item + " W/COUPON") == false
+      if coupon[:item] == item and coupon[:num] <= info[:count] and output[item + " W/COUPON"] == false
         cart[item][:count] -= coupon[:num]
         output[item + " W/COUPON"] = {
           :price => coupon[:cost],
@@ -34,8 +34,6 @@ def apply_coupons(cart, coupons)
           :count => 1
         }
 
-        
-      
       elsif output.key?(output[item + " W/COUPON"])
         output[item + " W/COUPON"][:count] += 1
       end
